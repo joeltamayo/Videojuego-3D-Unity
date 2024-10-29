eb5c54908f0306da7454bb40e52ea17b42604008
joel tamayo <jueltamayo25@gmail.com>
2024-10-28 23:14:13 -0600

    Controladores de la camara y la bola

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float impulseForce = 3f;
+    private bool ignoreNextcollision;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(ignoreNextcollision)
+        {
+            return;
+        }
+        rb.velocity = Vector3.zero;
+        rb.AddForce(Vector3.up * impulseForce, ForceMode.Impulse);
+
+        ignoreNextcollision = true;
+
+        Invoke("AllownextCollision", 0.2F);
+    }
+
+    private void AllownextCollision() {
+        ignoreNextcollision = false;
+    }
+}
---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public ballController ball;
+
+    private float offset;
+    void Start()
+    {
+        offset = transform.position.y - ball.transform.position.y;
+    }
+
+    void Update()
+    {
+        Vector3 actualPos = transform.position;
+        actualPos.y = ball.transform.position.y + offset;
+        transform.position = actualPos;
+    }
+}
