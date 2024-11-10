5139c4a68a859e045bc643e15d0f00d501d1dcdd
joel tamayo <jueltamayo25@gmail.com>
2024-11-10 15:30:22 -0600

    Efecto de Splash

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.UIElements;
 
 public class BallController : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class BallController : MonoBehaviour
     private bool ignoreNextcollision;
 
     private Vector3 startPosition;
-    
+
     [HideInInspector]
     public int perfectPass;
 
@@ -21,6 +22,8 @@ public class BallController : MonoBehaviour
 
     public int perfectPassCount = 3;
 
+    public GameObject splash;
+
     private void Start()
     {
         startPosition = transform.position;
@@ -28,6 +31,8 @@ public class BallController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+
+        addSplash(collision);
 
         if (ignoreNextcollision)
         {
@@ -58,12 +63,12 @@ public class BallController : MonoBehaviour
         isSuperSpeedActive = false;
     }
 
-    private void Update ()
+    private void Update()
     {
         if (perfectPass >= perfectPassCount && !isSuperSpeedActive)
         {
             isSuperSpeedActive = true;
-            rb.AddForce(Vector3.down*superSpeed, ForceMode.Impulse);
+            rb.AddForce(Vector3.down * superSpeed, ForceMode.Impulse);
         }
         //Debug.Log("Perfec pass:" + perfectPass);
         //Debug.Log("Super sped active:" + isSuperSpeedActive);
@@ -79,5 +84,17 @@ public class BallController : MonoBehaviour
     public void ResetBall()
     {
         transform.position = startPosition;
+    }
+
+    public void addSplash(Collision collision)
+    {
+        GameObject newSplash;
+
+        newSplash = Instantiate(splash);
+
+        newSplash.transform.SetParent(collision.transform);
+
+        newSplash.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.11f, this.transform.position.z);
+        Destroy(newSplash, 3);
     }
 }
