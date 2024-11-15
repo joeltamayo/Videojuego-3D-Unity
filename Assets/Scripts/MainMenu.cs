7f9018ccf479372ad755c6a6fcd95a5568811828
joel tamayo <jueltamayo25@gmail.com>
2024-11-15 12:24:09 -0600

    Actualizacion para regresar al menu en dispositivos moviles

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
     public void empezarJuego()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,4 +31,5 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Salir del juego");
     }
+
 }
